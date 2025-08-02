0a20ff640757b05c1c19326277d4630658974245
starry3085 <meko.h@qq.com>
2025-08-02 13:38:24 +0800

    Update deployment scripts - Remove manual GitHub Pages deployment (auto-deploy enabled)

--- push-all.bat
+++ push-all.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Quick deploy to both repositories...
+echo Pushing to all repositories...
 
 git add .
 git commit -m "Update: %date% %time%"
@@ -9,8 +9,11 @@ git push gitee main
 
 echo Pushing to GitHub...  
 git push github main
+
+echo Pushing to origin...
 git push origin main
 
-echo Done! Check:
+echo Done! 
 echo - Gitee: https://gitee.com/starry3085/work-reminder
-echo - GitHub Pages: https://starry3085.github.io/work-reminder/ (auto-deploying...)
+echo - GitHub: https://github.com/starry3085/work-reminder
+echo - GitHub Pages will auto-deploy: https://starry3085.github.io/work-reminder/
