42d382f0105beaf02eb09709fc92bbc8d50f6d5d
starry3085 <meko.h@qq.com>
2025-08-07 22:55:54 +0800

    chore: rename project to Hydrate Move and update all references
    - Update package.json name and repository URLs
    - Update README.md title and add repository links
    - Update index.html title and header
    - Update manifest.json app name
    - Update sitemap.xml and robots.txt URLs
    - Update version.js app name and version
    - Update LICENSE copyright
    
    - GitHub: https://github.com/starry3085/hydrate-move
    - Gitee: https://gitee.com/starry3085/hydrate-move

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // Simple version control
 window.APP_VERSION = '1.0.1';
-console.log('Office Wellness Reminder v' + window.APP_VERSION + ' - UI Fix Update');
+console.log('Hydrate Move v' + window.APP_VERSION + ' - UI Fix Update');
